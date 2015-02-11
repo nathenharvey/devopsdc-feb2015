a8752ee0008af68642b264e9fc10a89629225b6e
nathenharvey <nharvey@chef.io>
2015-02-11 00:12:36 -0500

    update serverspec tests to account for both platforms

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -10,12 +10,19 @@ describe 'apache::default' do
     expect(true).to eq true
   end
 
-  it 'is running' do
-    expect(service "httpd").to be_running
-  end
+  # the name of the service is platform dependent
+  # we could test that in serverspec but...
+  # it doesn't really matter.  what's important is
+  # that the home page is returned when called
+  # it 'is running' do
+  #   expect(service "httpd").to be_running
+  # end
 
   it 'listening on the correct port' do
     expect(port 80).to be_listening
   end
-
+  
+  it 'returns a valid home page' do
+    expect(command("wget http://localhost").exit_status).to eq 0
+  end
 end
