cbb47cf557836c4cb55bf11c9894e334f001f8ee
nathenharvey <nharvey@chef.io>
2015-02-10 23:10:50 -0500

    Add another context to the unit tests
    now testing centos and ubuntu

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -8,10 +8,10 @@ require 'spec_helper'
 
 describe 'apache::default' do
 
-  context 'When all attributes are default, on an unspecified platform' do
+  context 'When all attributes are default, on a centos platform' do
 
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new({:platform => "CentOS", :versision => "6.5"})
+      runner = ChefSpec::ServerRunner.new({:platform => "centos", :version => "6.5"})
       runner.converge(described_recipe)
     end
 
@@ -22,17 +22,21 @@ describe 'apache::default' do
     it 'installs apache' do
       expect(chef_run).to install_package "httpd"
     end
+  end
 
+  context 'When all attributes are default, on a ubunut platform' do
 
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new({:platform => "ubuntu", :version => "12.04"})
+      runner.converge(described_recipe)
+    end
 
+    it 'converges successfully' do
+      chef_run # This should not raise an error
+    end
 
-
-
-
-
-
-
-
-
+    it 'installs apache' do
+      expect(chef_run).to install_package "apache2"
+    end
   end
 end
