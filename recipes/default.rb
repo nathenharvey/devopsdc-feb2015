e24160b274002190c24600654f731d8aebad852d
nathenharvey <nharvey@chef.io>
2015-02-11 09:06:15 -0500

    Create a default home page
    kitchen will pass on both platforms now

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,9 +5,11 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 package_name = "httpd"
+document_root = "/var/www/html"
 if node["platform"] == "ubuntu"
   package_name = "apache2"
   include_recipe "apt::default"
+  document_root = "/var/www"
 end
 
 service_name = package_name
@@ -16,4 +18,11 @@ package "#{package_name}"
 
 service service_name do
   action :start
+end
+
+file "#{document_root}/index.html" do
+  content "<h1>Hello, world!</h1>"
+  owner "root"
+  group "root"
+  mode "0644"
 end
